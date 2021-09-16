39bca949f49f30fbcfa4863147c00c32ce78d02c
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-09-16 02:02:28 -0600

    Add files via upload

---
+++ NewClass_Udemy_3.java
@@ -0,0 +1,37 @@
+package javaapplication1;
+
+public class NewClass_Udemy_3 {
+
+    public static void main(String[] args) {
+        System.out.println(isWeekDay(6));
+    }
+
+    public static boolean isWeekDay(int x) {
+        switch (x) {
+            case 1:
+            // return true;
+            // break;
+            case 2:
+            // return true;
+            // break;
+            case 3:
+            // return true;
+            // break;
+            case 4:
+            // return true;
+            // break;
+            case 5:
+                return true;
+            // break;
+            case 6:
+            // return false;
+            // break;
+            case 7:
+                return false;
+            // break;
+            // return false;
+
+        }
+        return false;
+    }
+}
