0ff67944784b055a878da8c2a0dc692ef7d103f0
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-09-20 12:58:30 -0600

    Add files via upload

---
+++ Fan.java
@@ -0,0 +1,43 @@
+package com.siddhi.oops.level2;
+
+public class Fan {
+	// state
+	private String make;
+	private double radius;
+	private String color;
+	private boolean isOn;
+	private byte speed; // 0 to 5
+
+	// creation
+	public Fan(String make, double radius, String color) {
+		this.make = make;
+		this.radius = radius;
+		this.color = color;
+		// this.isOn = isOn;
+		// this.speed = speed;
+	}
+
+	// isOn
+	public void isOn(boolean isOn) {
+		this.isOn = isOn;
+	}
+
+	public void switchOn() {
+		this.isOn = true;
+		setSpeed((byte) 10);
+	}
+
+	public void switchOff() {
+		this.isOn = false;
+		setSpeed((byte) 0);
+	}
+
+	public void setSpeed(byte speed) {
+		this.speed = speed;
+	}
+
+	// print the state
+	public String toString() {
+		return String.format("make= %s, radius= %f, color= %s ,isOn= %b,speed= %d", make, radius, color, isOn, speed);
+	}
+}
