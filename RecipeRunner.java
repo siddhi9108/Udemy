4a9625d00de2a20435b11b3c26b5e1fe46b35774
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-09-22 09:24:02 -0600

    Add files via upload

---
+++ RecipeRunner.java
@@ -0,0 +1,13 @@
+package com.siddhi.oops.level2;
+
+public class RecipeRunner {
+
+	public static void main(String[] args) {
+		Recipe1 ob = new Recipe1();
+		ob.execute();
+
+		RecipeWithMicrowave ob1 = new RecipeWithMicrowave();
+		ob1.execute();
+	}
+
+}
