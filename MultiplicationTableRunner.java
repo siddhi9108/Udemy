fc163335f91116de03a47d1fba3242cb7ff7874c
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-10-08 11:41:55 -0600

    Add files via upload

---
+++ MultiplicationTableRunner.java
@@ -0,0 +1,14 @@
+package com.siddhi.firstjavaproject;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		// table.print(2,11,23);
+		table.print(8);
+		// System.out.println();
+		
+
+	}
+
+}
