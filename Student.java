05b8a234c9758b48ee7a4a98a444e0a5c3edd7c8
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-10-12 05:55:03 -0600

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,41 +1,76 @@
-package collections;
+package com.siddhi.arrays;
 
-public class Student implements Comparable<Student> {
-	private int id;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+
+public class Student {
+
 	private String name;
+	private ArrayList<Integer> arr = new ArrayList<Integer>();
 
-	public Student(int id, String name) {
-		super();
-		this.id = id;
+	public Student(String name, int... arr) {
 		this.name = name;
+		for (int mark : arr) {
+			this.arr.add(mark);
+		}
 	}
 
-	public int getId() {
-		return id;
+	public int getNumberOfMarks() {
+		// for (int mark : arr) {
+		// System.out.println(mark);
+		// }
+		return arr.size();
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public int getTotalSumOfMarks() {
+		int sum = 0;
+		for (int mark : arr) {
+			sum += mark;
+		}
+		// for (int i = 0; i < arr.size(); i++) {
+		// sum = sum + arr[i];
+		// System.out.println(sum);
+		// }
+		return sum;
 	}
 
-	public String getName() {
-		return name;
+	public int getMaxMarks() {
+		int max = Integer.MIN_VALUE;
+		for (int mark : arr) {
+			if (mark > max) {
+				max = mark;
+			}
+		}
+		return max;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public int getMinMarks() {
+		int min = Integer.MAX_VALUE;
+		for (int mark : arr) {
+			if (mark < min) {
+				min = mark;
+			}
+		}
+		return min;
+	}
+
+	public BigDecimal getAvgMarks() {
+		int sum = getTotalSumOfMarks();
+		int number = getNumberOfMarks();
+
+		return new BigDecimal(sum).divide(new BigDecimal(number), 3, RoundingMode.UP);
+	}
+
+	public void addNewMark(int mark) {
+		arr.add(mark);
 	}
 
 	public String toString() {
-		return id + " " + name;
+		return name + arr;
 	}
 
-	@Override
-	public int compareTo(Student that) {
-		// Object that;
-		// TODO Auto-generated method stub
-		// return 0;
-		return Integer.compare(that.id, this.id);
+	public void removeMarkAtIndex(int index) {
+		arr.remove(index);
 	}
-	
 }
