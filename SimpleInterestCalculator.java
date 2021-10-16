3ac1f4c1431ff15b34ac6183b3a0580f3c77561f
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-10-16 13:27:13 -0600

    Add files via upload

---
+++ SimpleInterestCalculator.java
@@ -0,0 +1,19 @@
+package com.siddhi.primitive.datatypes;
+
+import java.math.BigDecimal;
+
+public class SimpleInterestCalculator {
+	BigDecimal principal;
+	BigDecimal interest;
+
+	public SimpleInterestCalculator(String principal, String interest) {
+		this.principal = new BigDecimal(principal);
+		this.principal = new BigDecimal(interest).divide(new BigDecimal(100));
+	}
+
+	public BigDecimal calculateTotalValue(int noOfYears) {
+		BigDecimal totalValue = principal.add(principal.multiply(interest).multiply(new BigDecimal(noOfYears)));
+		return totalValue;
+	}
+
+}
